057c3197de05e0d55965f5a9cff17225ed3a75fa
Matthew Bradbury <MatthewJRBradbury@gmail.com>
2019-04-03 23:36:00 +0000

    cap letter

--- src/net/codejava/javaee/LoginServlet.java
+++ src/net/codejava/javaee/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/loginServlet")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
--- src/net/codejava/javaee/SuccessServlet.java
+++ src/net/codejava/javaee/SuccessServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/successServlet")
+@WebServlet("/SuccessServlet")
 public class SuccessServlet extends HttpServlet {
     
     protected void doGet(HttpServletRequest request,
