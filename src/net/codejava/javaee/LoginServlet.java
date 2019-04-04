475f05f96cf51412d336d6f1c216d69f07a8e29b
Matthew Bradbury <MatthewJRBradbury@gmail.com>
2019-04-04 00:09:45 +0000

    new dopost

--- src/net/codejava/javaee/LoginServlet.java
+++ src/net/codejava/javaee/LoginServlet.java
@@ -11,23 +11,6 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
-String userName = request.getParameter("userName");
-String password = request.getParameter("password");
-if ("admin".equalsIgnoreCase(userName) && "admin".equals(password)){
-	response.sendRedirect("SuccessServlet?user=" + userName);
-} else {
-	PrintWriter out = response.getWriter();
-	out.print("<html><head>");
-	out.print("<title>My First Servlet </title>");
-	out.print("</head>");
-        out.print("<body>");
-	out.print("The Entered Credentials does not match");
-	out.print("</body>");
-	out.print("</head>");
-}
-
-}
 protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 String userName = request.getParameter("userName");
 String password = request.getParameter("password");
--- src/net/codejava/javaee/SuccessServlet.java
+++ src/net/codejava/javaee/SuccessServlet.java
@@ -11,19 +11,6 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/SuccessServlet")
 public class SuccessServlet extends HttpServlet {
     
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        String user = request.getParameter("user");
-        PrintWriter out = response.getWriter();
-        out.print("<html><head>");
-        out.print("<title>My First Servlet</title>");
-        out.print("</head>");
-        out.print("<body>");
-        out.print("<h1>Hi Welcome " + user + "</h1>");
-        out.print("</body>");
-        out.print("</head>");
-    }
-
 protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         String user = request.getParameter("user");
